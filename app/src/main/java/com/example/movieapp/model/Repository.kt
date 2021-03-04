3b788ac5c4c018cccd289a177a66b0dfe198ccc0
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-03-04 11:44:01 +0300

    changing Repository and RepositoryImpl

--- app/src/main/java/com/example/movieapp/model/Repository.kt
+++ app/src/main/java/com/example/movieapp/model/Repository.kt
@@ -3,4 +3,5 @@ package com.example.movieapp.model
 interface Repository {
     fun getFilmsFromServer(): List<Film>
     fun getFilmsFromLocalStorage(): List<Film>
+    fun getFilmCategories(): Map<Categories,List<Film>>
 }
