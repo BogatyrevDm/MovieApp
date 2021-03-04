b226c893358e947fa2faa0111b707153cc492094
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-03-04 09:28:12 +0300

    correcting RecyclerViewAdapter

--- app/src/main/java/com/example/movieapp/view/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/movieapp/view/RecyclerViewAdapter.kt
@@ -5,16 +5,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.R
 import com.example.movieapp.model.Film
 
-class RecyclerViewAdapter() : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+class RecyclerViewAdapter(private var onItemViewClickListener: MainFragment.OnItemViewClickListener?) :
+    RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
     private var dataSource: List<Film> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val v: View = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
         return MyViewHolder(v)
+    }
+
+    fun removeListener() {
+        onItemViewClickListener = null
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -29,7 +35,7 @@ class RecyclerViewAdapter() : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHol
         dataSource = films
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun onBind(film: Film) {
             var titleTV = itemView.findViewById<TextView>(R.id.title)
             titleTV.text = film.title
@@ -43,6 +49,11 @@ class RecyclerViewAdapter() : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHol
             var iconIV = itemView.findViewById<AppCompatImageView>(R.id.poster)
             iconIV.setImageResource(R.drawable.screen)
 
+            itemView.findViewById<CardView>(R.id.card_view_root).setOnClickListener({
+                onItemViewClickListener?.onItemViewClick(film)
+            }
+
+            )
         }
     }
 }
