26298034716439c9092f17796cee72cc209b0072
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-03-07 22:07:53 +0300

    creating extension function to show snackbar

--- app/src/main/java/com/example/movieapp/view/MainFragment.kt
+++ app/src/main/java/com/example/movieapp/view/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +13,7 @@ import com.example.movieapp.R
 import com.example.movieapp.model.Film
 import com.example.movieapp.viewmodel.AppState
 import com.example.movieapp.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MainFragment : Fragment() {
 
@@ -19,7 +21,7 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private val viewModel: MainViewModel by lazy{ViewModelProvider(this).get(MainViewModel::class.java)}
+    private val viewModel: MainViewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
     private lateinit var recyclerView: RecyclerView
     private var adapter = RecyclerViewCategoriesAdapter(object : OnItemViewClickListener {
         override fun onItemViewClick(film: Film) {
@@ -68,11 +70,24 @@ class MainFragment : Fragment() {
             is AppState.Error -> {
                 adapter.setCategories(mapOf())
                 adapter.notifyDataSetChanged()
+                view?.findViewById<LinearLayout>(R.id.mainFragmentRootView)?.showSnackBar(
+                    getString(R.string.error),
+                    getString(R.string.reload),
+                    { viewModel.getDataFromLocalStorage() }
+                )
             }
             is AppState.Loading -> {
-
             }
         }
+    }
+
+    private fun View.showSnackBar(
+        text: String,
+        actionText: String,
+        action: (View) -> Unit,
+        length: Int = Snackbar.LENGTH_INDEFINITE
+    ) {
+        Snackbar.make(this, text, length).setAction(actionText, action).show()
     }
 
     interface OnItemViewClickListener {
