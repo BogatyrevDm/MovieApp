4f02d4a11bcc61cdbc3817d4dc9b720379150dc1
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-03-16 12:51:28 +0300

    creating DataUtils

---
+++ app/src/main/java/com/example/movieapp/utils/DataUtils.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.utils
+
+import com.example.movieapp.model.Film
+import com.example.movieapp.model.FilmDTO
+import com.example.movieapp.model.FilmSummary
+
+fun convertDTOToModel(filmDTO: FilmDTO): Film {
+    return Film(FilmSummary(filmDTO.id!!,filmDTO.title!!,
+        filmDTO.release_date!!,filmDTO.vote_average!!), filmDTO.original_title!!,
+        filmDTO?.overview!!,filmDTO.runtime!!,filmDTO.genres.toString(),
+        filmDTO.vote_count!!,filmDTO.budget!!,filmDTO.revenue!!)
+}
