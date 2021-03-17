2be1a2faabadafc69271bda6a5ae251a567aac42
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-03-17 15:50:56 +0300

    AppState refactoring

--- app/src/main/java/com/example/movieapp/viewmodel/AppState.kt
+++ app/src/main/java/com/example/movieapp/viewmodel/AppState.kt
@@ -1,10 +1,9 @@
 package com.example.movieapp.viewmodel
 
-import com.example.movieapp.model.Categories
 import com.example.movieapp.model.Film
 
 sealed class AppState {
-    data class Success(val movieData: Map<Categories, List<Film>>) : AppState()
+    data class Success(val movieData: List<Film>, val categoryPosition: Int) : AppState()
     data class Error(val error: Throwable) : AppState()
     object Loading : AppState()
 }
