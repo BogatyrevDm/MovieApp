044106027cc593e8619734d6b017b6b474b7aefa
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-03-17 15:48:47 +0300

    Repository refactoring

--- app/src/main/java/com/example/movieapp/repository/Repository.kt
+++ app/src/main/java/com/example/movieapp/repository/Repository.kt
@@ -2,9 +2,12 @@ package com.example.movieapp.repository
 
 import com.example.movieapp.model.Categories
 import com.example.movieapp.model.Film
+import com.example.movieapp.model.FilmDTO
+import com.example.movieapp.model.FilmsListDTO
+import retrofit2.Callback
 
 interface Repository {
-    fun getFilmsFromServer(): List<Film>
+    fun getFilmsFromServer(category: Categories, callback: Callback<FilmsListDTO>)
     fun getFilmsFromLocalStorage(): List<Film>
-    fun getFilmCategories(): Map<Categories,List<Film>>
+    fun getFilmCategories(): List<Film>
 }
--- app/src/main/java/com/example/movieapp/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/repository/RepositoryImpl.kt
@@ -2,15 +2,18 @@ package com.example.movieapp.repository
 
 import com.example.movieapp.model.Categories
 import com.example.movieapp.model.Film
-import com.example.movieapp.model.GetCategoriesList
+import com.example.movieapp.model.FilmsListDTO
+import retrofit2.Callback
 
-class RepositoryImpl : Repository {
-    override fun getFilmsFromServer(): List<Film> = listOf(Film(), Film(), Film())
+class RepositoryImpl(private val remoteDataSource: RemoteDataSource) : Repository {
+    override fun getFilmsFromServer(category: Categories, callback: Callback<FilmsListDTO>) {
+        remoteDataSource.getFilmsDetails(category, callback)
+    }
 
 
-    override fun getFilmsFromLocalStorage(): List<Film> =  listOf(Film(), Film(), Film())
+    override fun getFilmsFromLocalStorage(): List<Film> = listOf(Film(), Film(), Film())
 
 
-    override fun getFilmCategories(): Map<Categories, List<Film>> = GetCategoriesList()
+    override fun getFilmCategories(): List<Film> = listOf()
 
 }
