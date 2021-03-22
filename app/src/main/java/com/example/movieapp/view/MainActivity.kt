06a9799ae36606226995246786e5e29378e3b155
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-03-22 12:08:06 +0300

    adding menu into MainActivity

--- app/src/main/java/com/example/movieapp/view/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.movieapp.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movieapp.R
 
@@ -14,5 +16,25 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.menu_history_content -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, HistoryFragment.newInstance())
+                    .addToBackStack("")
+                    .commitAllowingStateLoss()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
